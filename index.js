5d88fa7af9ef408b053629534fe3d44c4c1aa417
Hee Jae Chae <poongnewga@gmail.com>
2017-10-27 21:09:43 +0900

    Install Passport for GoogleOAuth & Hide keys using .gitignore

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // commonJS - Node.js support commonJS
 // cf. React support ES2015 import module
 const express = require('express');
+// import Passport for Google OAuth
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// Make Route handler - Node 8 support Arrow Func.
-app.get('/', (req,res) => {
-  // respond with a simple JSON
-  res.send({bye: 'buddy'});
-});
+// Seperate Keys
+// clientID =
+// clientSecret =
+ 
+// Set Strategy
+passport.use(new GoogleStrategy());
+
+
+// // Make Route handler - Node 8 support Arrow Func.
+// app.get('/', (req,res) => {
+//   // respond with a simple JSON
+//   res.send({bye: 'buddy'});
+// });
+
+
 
 
 // Dynamic Port Binding using
