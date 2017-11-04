afbb9de60102a0b20b23578d259455b5468f8dcb
Hee Jae Chae <poongnewga@gmail.com>
2017-11-04 11:39:27 +0900

    Implementd Adding Credits for User

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req,res) => {
+    // check if user exists
+    if (!req.user) {
+      return res.status(401).send({error: "You Must Login!"});
+    }
+
     // with bodyparser, req.body is usable
     // console.log(req.body);
     const charge = await stripe.charges.create({
@@ -11,7 +16,10 @@ module.exports = app => {
       description: '$5 for 5 credits',
       source: req.body.id
     });
-
     // console.log(charge);
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
