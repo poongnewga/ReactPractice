a7ac9833e76c2dcef8b8e82059653537feaa2070
Hee Jae Chae <poongnewga@gmail.com>
2017-11-04 09:24:25 +0900

    Set Routers

--- client/src/index.js
+++ client/src/index.js
@@ -12,10 +12,3 @@ ReactDOM.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
 );
-
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
