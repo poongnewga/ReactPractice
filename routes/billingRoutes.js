7e3ee89c5d2c040fcb1b7fcdc353fcc6f9cc3919
Hee Jae Chae <poongnewga@gmail.com>
2017-11-04 11:44:26 +0900

    Add codes for checking Login

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,12 +1,9 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  app.post('/api/stripe', async (req,res) => {
-    // check if user exists
-    if (!req.user) {
-      return res.status(401).send({error: "You Must Login!"});
-    }
+  app.post('/api/stripe', requireLogin, async (req,res) => {
 
     // with bodyparser, req.body is usable
     // console.log(req.body);
